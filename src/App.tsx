19685cff82450fcdc1e5229c6a63eafd7d9e5084
Olena Solonikova <137723764+Olena3333@users.noreply.github.com>
2026-07-06 19:02:25 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import "./App.css";
-import CafeInfo from "./CafeInfo";
-import VoteOptions from "./VoteOptions";
-import VoteStats from "./VoteStats";
-import type { Votes, VoteType } from "../types/votes";
+import CafeInfo from "./components/CafeInfo";
+import VoteOptions from "./components/VoteOptions";
+import VoteStats from "./components/VoteStats";
+import type { Votes, VoteType } from "./types/votes";
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
